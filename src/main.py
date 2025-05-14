2ac5c4349406b10a5c8050a4462cf8bfe68956ef
Michela Rinaldo <rnmichela@gmail.com>
2025-05-14 17:03:05 +0200

    creation of html pages to list and create books and addition of view endpoints

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import argparse
+import os
 import sys
 import logging
 import sqlite3
 from api import api
 from view import view
-from flask import Flask
+from flask import Flask, json
 from typing import Dict
 from config import read_config
 from repository import BookRepository, ReviewRepository, ScoreRepository, ThemeRepository
@@ -12,11 +13,30 @@ from service import BookService, ReviewService, ScoreService, ThemeService
 
 logging.basicConfig(
     filename='app.log',
+    # detailed information, typically only of interest to a developer trying to diagnose a problem
     level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(filename)s - %(levelname)s - %(lineno)d - %(message)s'
 )
 
 app = Flask(__name__)
+
+def init_db(db_file):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    project_root = os.path.dirname(current_dir)
+
+    settings_path = os.path.join(project_root, "settings.json")
+
+    with open(settings_path, "r") as s:
+         settings = json.load(s)
+
+    schema_path = os.path.join(project_root, settings["schema_path"])
+
+    conn = sqlite3.connect(args.db_file, check_same_thread=False)
+
+    with open(schema_path, "r") as schema:
+            schema_script = schema.read()
+            conn.executescript(schema_script)
 
 def init_repositories(con) -> Dict[str,object]:
     return {
@@ -39,18 +59,37 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "")
 
     parser.add_argument('db_file', help='select your SQLite .db file')
+    parser.add_argument('--init-db', action='store_true', help='initialize empty .db file')
 
     args = parser.parse_args()
 
     config = read_config()
 
-    # conn = sqlite3.connect(sys.argv[1], check_same_thread=False)
+    if args.init_db:
+         init_db(args.db_file)
+
+    if not os.path.exists(args.db_file):
+         print("Attention, the selected file does not exist. Type --init-db to define an empty .db file.")
+         exit(1)
 
     conn = sqlite3.connect(args.db_file, check_same_thread=False)
 
-    with open("src/schema.sql", "r") as schema:
+    """
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    project_root = os.path.dirname(current_dir)
+
+    settings_path = os.path.join(project_root, "settings.json")
+
+    with open(settings_path, "r") as s:
+         settings = json.load(s)
+
+    schema_path = os.path.join(project_root, settings["schema_path"])
+
+    with open(schema_path, "r") as schema:
             schema_script = schema.read()
             conn.executescript(schema_script)
+    """
 
     respositories = init_repositories(conn)
 
--- src/view.py
+++ src/view.py
@@ -1,12 +1,62 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, current_app, redirect, render_template, request, url_for
+import requests
 
 view = Blueprint('view', __name__)
 
-@view.route('/template', methods=['GET'])
+@view.route('/', methods=['GET'])
 def show_base_template():
     return render_template('base.html')
+
+@view.route('/list_themes', methods=['GET'])
+def show_list_of_themes():
+
+    service = current_app.config['services']['theme']
+    themes = service.get_all_themes()
+
+    return render_template('theme/list.html', themes = themes)
 
 @view.route('/add_theme', methods=['GET'])
 def show_create_theme_form():
     return render_template('theme/create.html')
 
+@view.route('/list_books/<int:theme_id>', methods=['GET'])
+def show_list_of_books(theme_id):
+
+    service = current_app.config['services']['book']
+    books = service.get_all_books(theme_id)
+
+    return render_template('book/list.html', books = books, t_id = theme_id)
+
+@view.route('/add_book/<int:theme_id>', methods=['GET'])
+def show_create_book_form(theme_id):
+    return render_template('book/create.html', t_id = theme_id)
+
+@view.route('/process_book_form', methods=['POST'])
+def redirect_create_book_form():
+
+    author = request.form['author']
+    title  = request.form['title']
+    year   = request.form['year']
+    _id    = request.form['theme_id']
+
+    payload = {'author': author,
+               'title': title,
+               'publication_year': year,
+               'theme_id': int(_id) }
+
+    url = f"http://localhost:5001/books/{_id}"
+
+    try:
+        response = requests.post(url, json = payload)
+        return redirect(url_for('view.show_list_of_books', theme_id = _id))
+
+    except Exception as e:
+        return redirect(url_for('view.show_create_book_form', theme_id = _id))
+
+@view.route('/list_reviews/<int:book_id>', methods=['GET'])
+def show_list_of_reviews(book_id):
+
+    service = current_app.config['services']['review']
+    reviews = service.get_all_reviews(book_id)
+
+    return render_template('review/list.html', reviews = reviews)
